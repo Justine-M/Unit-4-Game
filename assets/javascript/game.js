4c82f65446aef9794da8aa226075817b8f902acf
Justine-M <justalexmays@hotmail.com>
2018-08-25 21:27:19 -0400

    last commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,97 +1,150 @@
-var targetNumber = 40;
+// Selects a random number to be shown at the start of the game
+// Number should be should be between 19 - 120
+var Random = Math.floor(Math.random() * 101 + 19)
 
-  $("#number-to-guess").text(targetNumber);
-  
+// Appending random number to the number-to-guess id in the html doc
+$('#number-to-guess').text(Random);
 
-  var counter = 0;
-  var wins = 0;
-  var losses = 0;
+// Each image number should be between 1-12
+var image1 = Math.floor(Math.random() * 11 + 1)
+var image2 = Math.floor(Math.random() * 11 + 1)
+var image3 = Math.floor(Math.random() * 11 + 1)
+var image4 = Math.floor(Math.random() * 11 + 1)
 
-  var totalScore = 0;
-  $("#your-total-score-is").text(totalScore);
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
 
-  function ResetGlobalVariables(){
-    //list of all the variables with original attributes here
-     totalScore = 0;
- }
+//   Check ID in Html
+$("#your-total-score-is").text(totalScore);
 
-  // Now for the hard part. Creating multiple crystals each with their own unique number value.
+//list of all the global variables you want reset here
+function ResetGlobalVariables() {
 
-  // We begin by expanding our array to include four options.
-  var numberOptions = [10, 6, 4, 8];
-  
-  // Next we create a for loop to create crystals for every numberOption.
-  for (var i = 0; i < numberOptions.length; i++) {
-
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "assets/images/crystal1.jpg", "src", "assets/images/crystal2.jpg", "src", "assets/images/crystal3.jpg", "src", "assets/images/crystal4.jpg");
-    //{attribute:value, attribute:value,...}
+    Random = Math.floor(Math.random() * 101 + 19);
+    console.log(Random)
+    $('#number-to-guess').text(Random);
+    totalScore = 0;
+    image1 = Math.floor(Math.random() * 11 + 1);
+    image2 = Math.floor(Math.random() * 11 + 1);
+    image3 = Math.floor(Math.random() * 11 + 1);
+    image4 = Math.floor(Math.random() * 11 + 1);
     
 
+}
 
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+// Create a click function for each crystal using their separate IDs
+$('#one').on('click', function () {
+    totalScore = totalScore + image1;
 
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
-
-    
-  }
-
-  // This time, our click event applies to every single crystal on the page. Not just one.
-  $(".crystal-image").on("click", function() {
-
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    totalScore += crystalValue;
-
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    //alert("New score: " + totalScore);
-
-    if (totalScore === targetNumber) {
+    if (totalScore === Random) {
         wins++;
         ResetGlobalVariables();
-		console.log("You won!");
+        console.log("You won!");
     }
-    
 
-    else if (totalScore >= targetNumber) {
+    if (totalScore >= Random) {
         losses++;
         ResetGlobalVariables();
         console.log("You lost!");
     }
 
-    
-
+    // Don't forget to input the html with the unit-4-game ID
     // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+
     var html =
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<p>Your Total Score is: " + totalScore + "</p>";
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Your Total Score is: " + totalScore + "</p>";
 
-   
 
-  // Set the inner HTML contents of the #game div to our html string
-  document.querySelector("#unit-4-game").innerHTML = html;
-  
-  
 
+    // Set the inner HTML contents of the #game div to our html string
+    document.querySelector("#unit-4-game").innerHTML = html;
+
+    $('#two').on('click', function () {
+        totalScore = totalScore + image2;
+
+        if (totalScore === Random) {
+            wins++;
+            ResetGlobalVariables();
+            console.log("You won!");
+        }
+
+        if (totalScore >= Random) {
+            losses++;
+            ResetGlobalVariables();
+            console.log("You lost!");
+        }
+
+        var html =
+            "<p>Wins: " + wins + "</p>" +
+            "<p>Losses: " + losses + "</p>" +
+            "<p>Your Total Score is: " + totalScore + "</p>";
+
+
+
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#unit-4-game").innerHTML = html;
+
+        $('#three').on('click', function () {
+            totalScore = totalScore + image3;
+
+            if (totalScore === Random) {
+                wins++;
+                ResetGlobalVariables();
+                console.log("You won!");
+            }
+
+            if (totalScore >= Random) {
+                losses++;
+                ResetGlobalVariables();
+                console.log("You lost!");
+            }
+
+            var html =
+                "<p>Wins: " + wins + "</p>" +
+                "<p>Losses: " + losses + "</p>" +
+                "<p>Your Total Score is: " + totalScore + "</p>";
+
+
+
+            // Set the inner HTML contents of the #game div to our html string
+            document.querySelector("#unit-4-game").innerHTML = html;
+
+            $('#four').on('click', function () {
+                totalScore = totalScore + image4;
+
+                if (totalScore === Random) {
+                    wins++;
+                    ResetGlobalVariables();
+                    console.log("You won!");
+                }
+
+                if (totalScore >= Random) {
+                    losses++;
+                    ResetGlobalVariables();
+                    console.log("You lost!");
+                }
+
+
+
+                var html =
+                    "<p>Wins: " + wins + "</p>" +
+                    "<p>Losses: " + losses + "</p>" +
+                    "<p>Your Total Score is: " + totalScore + "</p>";
+
+
+
+                // Set the inner HTML contents of the #game div to our html string
+                document.querySelector("#unit-4-game").innerHTML = html;
+
+            });
+
+        });
+    });
 });
+
 
 
 
